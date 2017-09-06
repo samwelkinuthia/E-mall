f7f1cdaecde49ef0f58b2b6b8e8f0cd22592d471
Victor Allen <vamuigua@gmail.com>
2017-09-06 11:36:15 +0300

    Modified js file

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,10 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require rails-ujs
-//= require turbolinks
-//= require toastr
 //= require jquery
+//= require bootstrap-sprockets
 //= require jquery_ujs
+//= require rails-ujs
+//= require toastr
+//= require turbolinks
 //= require_tree .
