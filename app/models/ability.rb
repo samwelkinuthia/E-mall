812e8a3dd69c0136938f19ede446694d5a2898ad
Victor Allen <vamuigua@gmail.com>
2017-09-06 12:28:03 +0300

    Update admin and user ability

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,11 +3,13 @@ class Ability
 
   def initialize(user)
     # Define abilities for the passed in user here. For example:
-      user ||= User.new # guest user (not logged in)
-      if user.admin?
-        can :manage, :all
-      else
-        can :read, :all
-      end
+    user ||= User.new # guest user (not logged in)
+    if user.admin?
+      can :manage, :all
+      can :access, :rails_admin       # only allow admin users to access Rails Admin
+      can :dashboard                  # allow access to dashboard
+    else
+      can :read, :all
+    end
   end
 end
