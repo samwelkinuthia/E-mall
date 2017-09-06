4dbc36ad6b99bedb184f90669e8e885f60e69cae
Victor Allen <vamuigua@gmail.com>
2017-09-06 17:28:59 +0300

    Added code to fetch the address and coordinates of a mall

--- app/models/mall.rb
+++ app/models/mall.rb
@@ -1,2 +1,6 @@
 class Mall < ApplicationRecord
+  #Find address of mall and use it for fetching the Longitude and Latitude
+  geocoded_by :address
+  #After finding address find the mall's location
+  after_validation :geocode, if: :address_changed?
 end
