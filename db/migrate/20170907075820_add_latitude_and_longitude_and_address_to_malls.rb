8f3013d30edcd9c240dc09bff130859b033e646e
samwel <samwuelkinuthia@gmail.com>
2017-09-07 14:27:49 +0300

    removed extra migration file

--- db/migrate/20170907075820_add_latitude_and_longitude_and_address_to_malls.rb
+++
@@ -1,7 +0,0 @@
-class AddLatitudeAndLongitudeAndAddressToMalls < ActiveRecord::Migration[5.1]
-  def change
-    add_column :malls, :address, :string
-    add_column :malls, :latitude, :float
-    add_column :malls, :longitude, :float
-  end
-end
