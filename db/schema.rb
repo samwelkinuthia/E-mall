16172e71fea45651b4efc1f2eec4a99e6f70f6f9
samwel <samwuelkinuthia@gmail.com>
2017-09-07 11:04:17 +0300

    created address input and migrated malls

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170906090957) do
+ActiveRecord::Schema.define(version: 20170907075820) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,9 @@ ActiveRecord::Schema.define(version: 20170906090957) do
     t.boolean "rooms_status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "address"
+    t.float "latitude"
+    t.float "longitude"
   end
 
   create_table "users", force: :cascade do |t|
