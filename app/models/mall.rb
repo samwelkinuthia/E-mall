1c88f263e1d1a8444edb4ebd6e708ff0aa64eb48
samwel <samwuelkinuthia@gmail.com>
2017-09-07 16:40:57 +0300

    set relationship with mall attachments

--- app/models/mall.rb
+++ app/models/mall.rb
@@ -2,4 +2,7 @@ class Mall < ApplicationRecord
   geocoded_by :address
 
   after_validation :geocode, if: :address_changed?
+
+  has_many :mall_attachments
+  accepts_nested_attributes_for :mall_attachments
 end
