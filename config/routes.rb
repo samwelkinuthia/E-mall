b2f2b0cda6e24ba027a00c7995a9b697361923aa
samwel <samwuelkinuthia@gmail.com>
2017-09-11 16:42:06 +0300

    scaffold rooms

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :rooms
   root "malls#index"
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
---
+++ db/migrate/20170911134152_create_rooms.rb
@@ -0,0 +1,22 @@
+class CreateRooms < ActiveRecord::Migration[5.1]
+  def change
+    create_table :rooms do |t|
+      t.string :name
+      t.text :description
+      t.integer :room_number
+      t.float :area
+      t.string :wing
+      t.integer :floor
+      t.time :opening_time
+      t.time :closing_time
+      t.bigint :phone
+      t.string :email
+      t.string :facebook
+      t.string :instagram
+      t.string :twitter
+      t.boolean :occupied
+
+      t.timestamps
+    end
+  end
+end
