bbb0f4f5aebcb0ba38040e5c854130226b8ad7a3
NajibOsman <machidydemajka38@gmail.com>
2017-09-12 10:37:15 +0300

    Modified the routes with events routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :events
   match '/contacts',     to: 'contacts#new',             via: 'get'
   resources "contacts", only: [:new, :create]
   resources :contacts
