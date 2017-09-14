a155b4060668b962bdf59323b93c8d9054171462
NajibOsman <machidydemajka38@gmail.com>
2017-09-14 22:42:22 +0300

    Configuered in the development,rb for mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,6 +32,21 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "myapp.herokuapp.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV["Abdulmajid_Osman"],
+    password: ENV["MANCHIZY"]
+  }
+
+
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
