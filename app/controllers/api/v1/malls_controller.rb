c5e19aedafc82c1392f21714daf44bd68b6ebc2e
Victor Allen <vamuigua@gmail.com>
2017-09-27 08:48:42 +0300

    MOdified response of status

--- app/controllers/api/v1/malls_controller.rb
+++ app/controllers/api/v1/malls_controller.rb
@@ -4,40 +4,36 @@ module Api
       respond_to :json
       def index
         malls = Mall.order('created_at DESC');
-        render json: {status: 'SUCCESS',message:'Loaded malls', data:malls},status: :ok
+        render json: {status: 'SUCCESS',message:'Loaded malls', data:malls},status: 200
       end
       def show
-      	mall = Mall.find(params[:id])
-      	render json: {status: 'SUCCESS',message:'Loaded mall', data:mall},status: :ok
+        mall = Mall.find(params[:id])
+        render json: {status: 'SUCCESS',message:'Loaded mall', data:mall},status: 200
       end
       def create
-      	mall = Mall.new(mall_params)
-      	if mall.save
-      		render json: {status: 'SUCCESS',message:'Saved mall', data:mall},status: :ok
-      	else
-      		render json: {status: 'ERROR',message:'Mall not saved', data:mall.errors},status: :unprocessable_entity
-      	end
+        mall = Mall.new(mall_params)
+        if mall.save
+          render json: {status: 'SUCCESS',message:'Saved mall', data:mall},status: 201
+        else
+          render json: {status: 'ERROR',message:'Mall not saved', data:mall.errors},status: :unprocessable_entity
+        end
       end
-
       def update
-      	mall = Mall.find(params[:id])
-      	if mall.update_attributes(mall_params)
-      		render json: {status: 'SUCCESS',message:'Updated mall', data:mall},status: :ok
-      	else
-      		render json: {status: 'ERROR',message:'Mall not updated', data:mall.errors},status: :unprocessable_entity
-      	end
+        mall = Mall.find(params[:id])
+        if mall.update_attributes(mall_params)
+          render json: {status: 'SUCCESS',message:'Updated mall', data:mall},status: 200
+        else
+          render json: {status: 'ERROR',message:'Mall not updated', data:mall.errors},status: :unprocessable_entity
+        end
       end
-
       def destroy
-      	mall = Mall.find(params[:id])
-      	mall.destroy
-      	render json: {status: 'SUCCESS',message:'Deleted mall', data:mall},status: :ok
+        mall = Mall.find(params[:id])
+        mall.destroy
+        render json: {status: 'SUCCESS',message:'Deleted mall', data:mall},status: 200
       end
-
       private
-
       def mall_params
-      	params.permit(:name, :mallpicture, :address, :total_rooms, :parking_space, :description, :email, :phone_no, :opening_time, :closing_time, :rooms_status, mall_attachments_attributes: %i[id mall_id avatar])
+        params.permit(:name, :mallpicture, :address, :total_rooms, :parking_space, :description, :email, :phone_no, :opening_time, :closing_time, :rooms_status, mall_attachments_attributes: %i[id mall_id avatar])
       end
     end
   end
