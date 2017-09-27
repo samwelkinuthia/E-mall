c976ad909cd84b5710fe6586ea052ad936ea5cde
Victor Allen <vamuigua@gmail.com>
2017-09-27 08:50:13 +0300

    Added routes for rooms

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
-  
+
   root "home#index"
 
   get "/pages/:page" => "pages#show"
@@ -28,7 +28,7 @@ Rails.application.routes.draw do
       end
     end
   end
-  
+
   namespace :api, defaults:{format: 'json'} do
     namespace :v1 do
       resources :malls do
