5b36284fe2c60157cfbae16185a81d38a07211ff
Victor Allen <vamuigua@gmail.com>
2017-09-27 07:56:00 +0300

    Added gems and malls api controller

--- config/routes.rb
+++ config/routes.rb
@@ -28,5 +28,12 @@ Rails.application.routes.draw do
       end
     end
   end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  namespace :api, defaults:{format: 'json'} do
+    namespace :v1 do
+      resources :malls do
+        resources :rooms
+      end
+    end
+  end
 end
