3509e53975bd9a2fd5a2a3cdac1cbdfce34e98b8
Victor Allen <vamuigua@gmail.com>
2017-09-27 08:49:47 +0300

    Imported Concerns

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::Base
+  #imported Concerns
+  include Response
+  include ExceptionHandler
+
   protect_from_forgery with: :exception
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to  main_app.root_url, :alert => exception.message
