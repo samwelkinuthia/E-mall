63d3090d8772ca0de5915cd42b6821080369663b
samwel <samwuelkinuthia@gmail.com>
2017-10-01 14:51:21 +0300

    styling update all

--- db/migrate/20170911134316_create_rooms.rb
+++ db/migrate/20170911134316_create_rooms.rb
@@ -1,20 +1,20 @@
 class CreateRooms < ActiveRecord::Migration[5.1]
   def change
     create_table :rooms do |t|
-      t.string :name, default: "room name"
-      t.text :description, default: "room description"
+      t.string :name
+      t.text :description
       t.integer :room_number, default: 0
-      t.float :area, default: 300
-      t.string :wing, default: "Wing"
+      t.float :area, default: 1000
+      t.string :wing, default: "Left"
       t.integer :floor, default: 1
-      t.time :opening_time, default: Time.now
-      t.time :closing_time, default: Time.now
-      t.bigint :phone, default: 0
-      t.string :email, default: "room@gmail.com"
-      t.string :facebook, default: "facebook.com"
-      t.string :instagram, default: "room.instagram.com"
-      t.string :twitter, default: "room.twitter.com"
-      t.integer :rent, default: 0
+      t.time :opening_time
+      t.time :closing_time
+      t.bigint :phone
+      t.string :email
+      t.string :facebook
+      t.string :instagram
+      t.string :twitter
+      t.integer :rent, default: 50000
       t.boolean :occupied, default: false
       t.belongs_to :mall, foreign_key: true
 
